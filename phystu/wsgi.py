37e40b3a3b25de2f37b186675a2734e5634f8fbf
Lizi1010 <30461912+Lizi1010@users.noreply.github.com>
2017-08-17 13:16:24 +0800

    web

--- phystu/wsgi.py
+++ phystu/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for phystu project.
+WSGI config for djangoweb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phystu.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoweb.settings")
 
 application = get_wsgi_application()
